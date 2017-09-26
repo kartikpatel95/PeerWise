dc849c72b1068006fa7efaeada5eac697b208e6b
Vincent Nio <vnio666@aucklanduni.ac.nz>
2017-09-26 17:23:12 +1300

    fix answer and added tool tips

--- scripts/achievementsTooltips.js
+++ scripts/achievementsTooltips.js
@@ -1,5 +1,17 @@
-var achievement_1 = document.getElementById('achievement_1');
-achievement_1.title = "tester";
+document.getElementById('achievement_1').title = "Reach level 2!";
+document.getElementById('achievement_2').title = "Create first question!";
+document.getElementById('achievement_3').title = "Answer first question!";
+document.getElementById('achievement_4').title = "Write 5 questions!";
+document.getElementById('achievement_5').title = "30 day streak!";
+document.getElementById('achievement_6').title = "2 hour marathon!";
+document.getElementById('achievement_7').title = "Secret!";
+document.getElementById('achievement_8').title = "Answer all!";
+document.getElementById('achievement_9').title = "Non modivated!";
+document.getElementById('achievement_10').title = "Answer 500 question!";
+document.getElementById('achievement_11').title = "TBD";
+document.getElementById('achievement_12').title = "TBD";
+
+
 
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
